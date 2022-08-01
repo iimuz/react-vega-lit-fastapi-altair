78899cbdf832b4b6a565ff9172ed4d3505b2e51f
iimuz <iz.primary.story+github@gmail.com>
2022-08-01 16:29:04 +0900

    fix: バックエンドからAPIでデータを取得して表示するように修正

--- src/til-20220801/frontend/src/main.tsx
+++ src/til-20220801/frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = (): JSX.Element => {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    getHello()
+      .then((msg) => setMessage(msg))
+      .catch((err) => console.log(err));
+  }, [message]);
+
   return (
     <div>
-      <h1>Hello React!</h1>
+      <h1>{message}</h1>
     </div>
   );
 };
@@ -14,3 +21,11 @@ if (!container) throw new Error('Failed to find the root element');
 const root = createRoot(container);
 root.render(<App />);
 
+async function getHello(): Promise<string> {
+  const res = await fetch('http://localhost:8000/');
+  if (!res.ok) return 'error';
+  const data = await res.json();
+  const message = data['message'];
+
+  return message;
+}
