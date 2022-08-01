a6bf8ae13f8f235637306058b1c7fd752ce6af33
iimuz <iz.primary.story+github@gmail.com>
2022-08-01 17:47:26 +0900

    fix: backendからグラフ描画のspecを受け取り、描画する部分を追加

--- src/til-20220801/frontend/src/main.tsx
+++ src/til-20220801/frontend/src/main.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import embed, { VisualizationSpec } from 'vega-embed';
+
+const HOST = 'http://localhost:8000';
 
 const App = (): JSX.Element => {
   const [message, setMessage] = useState('');
@@ -7,11 +10,16 @@ const App = (): JSX.Element => {
     getHello()
       .then((msg) => setMessage(msg))
       .catch((err) => console.log(err));
+    getSimpleLineJson()
+      .then((data) => embed('#vis', data))
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }, [message]);
 
   return (
     <div>
       <h1>{message}</h1>
+      <div id='vis'></div>
     </div>
   );
 };
@@ -22,10 +30,18 @@ const root = createRoot(container);
 root.render(<App />);
 
 async function getHello(): Promise<string> {
-  const res = await fetch('http://localhost:8000/');
+  const res = await fetch(HOST);
   if (!res.ok) return 'error';
   const data = await res.json();
   const message = data['message'];
 
   return message;
+}
+
+async function getSimpleLineJson(): Promise<VisualizationSpec> {
+  const res = await fetch(HOST + '/simple-line');
+  if (!res.ok) return {};
+  const spec: VisualizationSpec = await res.json();
+
+  return spec;
 }
