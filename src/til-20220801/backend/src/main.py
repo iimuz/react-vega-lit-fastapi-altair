ccb1ab42a44a8a2a88ed01827d2f7fd42b500dd9
iimuz <iz.primary.story+github@gmail.com>
2022-08-01 17:46:24 +0900

    fix: altairが出力したjsonデータを返すAPIを追加

--- src/til-20220801/backend/src/main.py
+++ src/til-20220801/backend/src/main.py
@@ -1,5 +1,11 @@
+import json
+from io import StringIO
+
+import altair as alt
+import numpy as np
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pandas import DataFrame
 
 app = FastAPI()
 app.add_middleware(
@@ -13,4 +19,18 @@ app.add_middleware(
 
 @app.get("/")
 def root():
+    """API動作確認用に単なるメッセージを返す."""
     return {"message": "Hello World"}
+
+
+@app.get("/simple-line")
+def simple_line():
+    """sin波を作成してvega-liteのjson形式で返す."""
+    x = np.arange(100)
+    df = DataFrame({"x": x, "y": np.sin(x / 5)})
+    chart = alt.Chart(df).mark_line().encode(x="x", y="y")
+
+    buff = StringIO()
+    chart.save(buff, format="json")
+
+    return json.loads(buff.getvalue())
