f0aa4f872c070034ac99174f2752d6e6a722323d
iimuz <iz.primary.story+github@gmail.com>
2022-08-03 17:05:26 +0900

    feat: dataのみ返すAPIを追加

--- src/til-20220801/backend/src/main.py
+++ src/til-20220801/backend/src/main.py
@@ -1,10 +1,12 @@
 import json
 from io import StringIO
+from typing import Tuple
 
 import altair as alt
 import numpy as np
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from numpy import ndarray
 from pandas import DataFrame
 
 app = FastAPI()
@@ -26,11 +28,27 @@ def root():
 @app.get("/simple-line")
 def simple_line():
     """sin波を作成してvega-liteのjson形式で返す."""
-    x = np.arange(100)
-    df = DataFrame({"x": x, "y": np.sin(x / 5)})
+    x, y = _line_data(100)
+    df = DataFrame({"x": x, "y": y})
     chart = alt.Chart(df).mark_line().encode(x="x", y="y")
 
     buff = StringIO()
     chart.save(buff, format="json")
 
     return json.loads(buff.getvalue())
+
+
+@app.get("/simple-line-data")
+def simple_line_data():
+    """sin波を作成してdataのみ返す."""
+    x, y = _line_data(100)
+
+    return {"x": x.tolist(), "y": y.tolist()}
+
+
+def _line_data(num: int) -> Tuple[ndarray, ndarray]:
+    """sin波を生成する."""
+    x = np.arange(num)
+    y = np.sin(x / 5)
+
+    return (x, y)
